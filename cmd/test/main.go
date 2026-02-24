0d5ffc220643b3903c2f9eabef854253af8a5c73
Chawanangwa Chipeta <chawanangwachipeta@Chawanangwas-MacBook-Air.local>
2026-02-24 10:23:51 +0200

    Chore(Main.go)Commented out the Main Go FIle with package main

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,33 +1,33 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"log"
-	"payrail"
-	"payrail/core"
-)
+// import (
+// 	"fmt"
+// 	"log"
+// 	"payrail"
+// 	"payrail/core"
+// )
 
-func main() {
-	client, err := payrail.NewClient("f423b2e4be7e45bf63e378e12baf5d56d0781ddc094aa2884764759a9bc68984", "lenco")
-	if err != nil {
-		log.Fatal(err)
-	}
+// func main() {
+// 	client, err := payrail.NewClient("f423b2e4be7e45bf63e378e12baf5d56d0781ddc094aa2884764759a9bc68984", "lenco")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	req := core.ChargeRequest{
-		Amount:    0.1,
-		Country:   core.CountryZM,
-		Reference: "test-ref-123",
-		Phone:     "+260769312808",
-		Operator:  core.OperatorMTN,
-		Bearer:    core.BearerCustomer,
-	}
+// 	req := core.ChargeRequest{
+// 		Amount:    0.1,
+// 		Country:   core.CountryZM,
+// 		Reference: "test-ref-123",
+// 		Phone:     "+260769312808",
+// 		Operator:  core.OperatorMTN,
+// 		Bearer:    core.BearerCustomer,
+// 	}
 
-	resp, err := client.Charge(req)
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	resp, err := client.Charge(req)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	fmt.Println("Status:", resp.Status)
-	fmt.Println("Reference:", resp.Reference)
-	fmt.Println("Raw:", string(resp.RawResponse))
-}
+// 	fmt.Println("Status:", resp.Status)
+// 	fmt.Println("Reference:", resp.Reference)
+// 	fmt.Println("Raw:", string(resp.RawResponse))
+// }
